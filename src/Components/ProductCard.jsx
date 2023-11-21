effcff28bd567ea057c63bced84120101e0ddbd1
kumaresan2001 <kumaresancrseven@gmail.com>
2023-11-21 19:21:00 +0530

    new change

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,24 +1,28 @@
 function ProductCard({ item }) {
   return (
     // <div className="app">
-    <div className="col-lg-4 mt-4">
-      <div className="card card-prod" style={{ width: "18em" }}>
-        <img
-          className="card-img"
-          src={item.image}
-          alt="Product image"
-          width={"80px"}
-          height="200px"
-        />
-        <div className="p-2 text-center">
-          <h2 className="p-2">{item.title}</h2>
-          <h4 className="p-1">&#8377; {item.price}</h4>
-          <h3 className="p-1"> {item.category}</h3>
-          <div className="p-1">{item.description}</div>
-          <h3 className="p-1">Sold: {item.sold === true ? "true" : "false"}</h3>
+    <>
+      <div className="col-lg-4 mt-4">
+        <div className="card card-prod" style={{ width: "18em" }}>
+          <img
+            className="card-img"
+            src={item.image}
+            alt="Product image"
+            width={"80px"}
+            height="200px"
+          />
+          <div className="p-2 text-center">
+            <h2 className="p-2">{item.title}</h2>
+            <h4 className="p-1">&#8377; {item.price}</h4>
+            <h3 className="p-1"> {item.category}</h3>
+            <div className="p-1">{item.description}</div>
+            <h3 className="p-1">
+              Sold: {item.sold === true ? "true" : "false"}
+            </h3>
+          </div>
         </div>
       </div>
-    </div>
+    </>
     // </div>
   );
 }
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -6,6 +6,8 @@ import ProductCard from "./ProductCard";
 
 function Products() {
   const [product, setProduct] = useState([]);
+  const [search, setSearch] = useState("");
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     Prod();
@@ -20,11 +22,68 @@ function Products() {
   };
   return (
     <div className="container">
+      <input
+        className="input"
+        placeholder="Search Products"
+        onChange={(e) => setSearch(e.target.value)}
+      ></input>
+
+      <div className="price">
+        <h2>
+          Price :
+          <div className="btn">
+            <button className="mybtn-low" onClick={() => setCount(1)}>
+              Low to High
+            </button>
+          </div>
+          <div className="btn ">
+            <button
+              className="mybtn-low"
+              type="button"
+              onClick={() => setCount(-1)}
+            >
+              High to Low
+            </button>
+          </div>
+          <div className="btn">
+            <button
+              className="mybtn-low"
+              type="button"
+              onClick={() => setCount(0)}
+            >
+              Reset
+            </button>
+          </div>
+        </h2>
+      </div>
+
       <div className="row">
         <>
-          {product.map((items, index) => {
-            return <ProductCard key={index} item={items} />;
-          })}
+          {product
+            .sort((a, b) => {
+              if (count == 0) {
+                return 0;
+              } else if (count == 1) {
+                return a.price - b.price;
+              } else if (count == -1) {
+                return b.price - a.price;
+              }
+            })
+            .filter((item) => {
+              return search.toLowerCase() === ""
+                ? item
+                : item.title.toLowerCase().includes(search.toLowerCase()) ||
+                    item.description
+                      .toLowerCase()
+                      .includes(search.toLowerCase()) ||
+                    item.category
+                      .toLowerCase()
+                      .includes(search.toLowerCase()) ||
+                    String(item.price).includes(search);
+            })
+            .map((items, index) => {
+              return <ProductCard key={index} item={items} />;
+            })}
         </>
       </div>
     </div>
